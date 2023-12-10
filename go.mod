94ddfc07ba0f4819103fc082647ab958a0ea1367
Brett Logan <lindluni@github.com>
2023-12-10 12:17:22 -0500

    Create extension for updating release body
    Signed-off-by: Brett Logan <lindluni@github.com>

--- go.mod
+++ go.mod
@@ -1,23 +1,15 @@
-module github.com/lindluni/gh-update-releases
+module github.com/lindluni/gh-update-release
 
-go 1.21.0
-
-require github.com/cli/go-gh/v2 v2.4.0
+go 1.21.5
 
 require (
-	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
-	github.com/cli/safeexec v1.0.0 // indirect
-	github.com/cli/shurcooL-graphql v0.0.4 // indirect
-	github.com/henvic/httpretty v0.0.6 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/muesli/termenv v0.13.0 // indirect
-	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/google/go-github/v57 v57.0.0
+	github.com/urfave/cli/v2 v2.26.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
--- main.go
+++ main.go
@@ -1,26 +1,200 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"strings"
 
-	"github.com/cli/go-gh/v2/pkg/api"
+	"github.com/google/go-github/v57/github"
+	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	fmt.Println("hi world, this is the gh-update-releases extension!")
-	client, err := api.DefaultRESTClient()
-	if err != nil {
-		fmt.Println(err)
-		return
+func StringPrompt(prompt string) string {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf("%s: ", prompt)
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
+	return strings.TrimSpace(s)
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+func getRelease(ctx context.Context, client *github.Client, owner, repo, tag string) (*github.RepositoryRelease, error) {
+	release, resp, err := client.Repositories.GetReleaseByTag(ctx, owner, repo, tag)
+	if err != nil {
+		if resp.StatusCode == http.StatusNotFound {
+			return nil, fmt.Errorf("release %s not found", tag)
+		}
+
+		return nil, fmt.Errorf("failed to get release %s: %w", tag, err)
+	}
+
+	return release, nil
+}
+
+func getAllReleases(ctx context.Context, client *github.Client, owner, repo string) ([]*github.RepositoryRelease, error) {
+	var allReleases []*github.RepositoryRelease
+	opts := &github.ListOptions{
+		PerPage: 100,
+	}
+	for {
+		releases, resp, err := client.Repositories.ListReleases(ctx, owner, repo, opts)
+		if err != nil {
+			if resp.StatusCode == http.StatusNotFound {
+				return nil, fmt.Errorf("repository %s/%s not found", owner, repo)
+			}
+
+			return nil, fmt.Errorf("failed to list releases: %w", err)
+		}
+		allReleases = append(allReleases, releases...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+
+	return allReleases, nil
+}
+
+func updateReleaseBody(ctx context.Context, client *github.Client, release *github.RepositoryRelease, owner, repo, value, replacement string) error {
+	body := strings.ReplaceAll(release.GetBody(), value, replacement)
+	if strings.Compare(body, release.GetBody()) == 0 {
+		fmt.Printf("%s: No changes to release body found, skipping\n", release.GetTagName())
+		return nil
+	}
+
+	release.Body = &body
+	fmt.Printf("%s: Updating release body\n", release.GetTagName())
+	_, _, err := client.Repositories.EditRelease(ctx, owner, repo, release.GetID(), release)
+	if err != nil {
+		return fmt.Errorf("%s: failed to update release body: %w", release.GetTagName(), err)
+	}
+
+	return nil
+}
+
+func run(ctx *cli.Context) error {
+	owner := ctx.String("owner")
+	repo := ctx.String("repo")
+	value := ctx.String("value")
+	replacement := ctx.String("replacement")
+	token := ctx.String("token")
+	all := ctx.Bool("all")
+	tag := ctx.String("release")
+
+	if all && tag != "" {
+		return fmt.Errorf("cannot specify both --all and --release")
+	}
+
+	if !all && tag == "" {
+		return fmt.Errorf("must specify either --all or --release")
+	}
+
+	if all {
+		response := StringPrompt("Are you sure you want to update all releases? (y/n)")
+		if response != "y" {
+			fmt.Println("Invalid response, aborting")
+			return nil
+		}
+
+		ctx := context.Background()
+		client := github.NewClient(nil).WithAuthToken(token)
+		fmt.Println("Fetching all releases")
+		releases, err := getAllReleases(ctx, client, owner, repo)
+		if err != nil {
+			return err
+		}
+
+		for _, release := range releases {
+			fmt.Printf("%s: Checking for updates to release body\n", release.GetTagName())
+			err := updateReleaseBody(ctx, client, release, owner, repo, value, replacement)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if tag != "" {
+		response := StringPrompt(fmt.Sprintf("Are you sure you want to update release %s? (y/n)", tag))
+		if response != "y" {
+			fmt.Println("Invalid response, aborting")
+			return nil
+		}
+
+		ctx := context.Background()
+		client := github.NewClient(nil).WithAuthToken(token)
+		release, err := getRelease(ctx, client, owner, repo, tag)
+		if err != nil {
+			return err
+		}
+
+		return updateReleaseBody(ctx, client, release, owner, repo, value, replacement)
+	}
+
+	return nil
+}
+
+func main() {
+	app := &cli.App{
+		Name:                 "update-release",
+		Usage:                "Update strings in GitHub releases",
+		Version:              "1.0.0",
+		EnableBashCompletion: true,
+		Suggest:              true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "owner",
+				Aliases:  []string{"o"},
+				Usage:    "GitHub repository organization or owner",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "repo",
+				Aliases:  []string{"r"},
+				Usage:    "GitHub repository name",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "value",
+				Aliases:  []string{"l"},
+				Usage:    "Value to replace",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "replacement",
+				Aliases:  []string{"p"},
+				Usage:    "Replacement value",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "token",
+				Aliases:  []string{"t"},
+				Usage:    "GitHub personal access token",
+				Required: true,
+			},
+			&cli.BoolFlag{
+				Name:    "all",
+				Aliases: []string{"a"},
+				Usage:   "Update all releases",
+			},
+			&cli.StringFlag{
+				Name:    "release",
+				Aliases: []string{"s"},
+				Usage:   "Update a specific release",
+			},
+		},
+		Action: run,
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
